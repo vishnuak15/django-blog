dcef805d37a90a34851796befaf857d3cf5f0d27
Vishnu <vishnuak15@gmail.com>
2021-08-24 21:03:05 +0530

    updated profile page

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 # Create your views here.
+from .forms import *
 
 def register(request):
     if request.method == "POST":
@@ -19,4 +20,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method == "POST":    
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form':u_form,
+        'p_form':p_form,
+    }
+    return render(request,'users/profile.html',context)
