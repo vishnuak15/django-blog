dc02d86903ce1f5ea2a97b5253696af9252581e9
Vishnu <vishnuak15@gmail.com>
2021-08-24 11:34:17 +0530

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from .models import Post
+
+def home(request):
+    context = {
+        'posts': Post.objects.all(),
+    }
+    return render(request,'blog/home.html',context)
+
+
+def about(request):
+    return render(request,'blog/about.html',{'title': 'About'})
+
+
+def login(request):
+    return render(request,'blog/.html')
---
+++ users/views.py
@@ -0,0 +1,22 @@
+from users.forms import UserResgistrForm
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserResgistrForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request,f'Your Account has been created!, you for are Now able to login as {username}!')
+            return redirect('login')
+    else:
+        form = UserResgistrForm()
+    return render(request,'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
