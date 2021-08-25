d2ca5173d10729a5348f66d9df7443858d91f05f
Vishnu <vishnuak15@gmail.com>
2021-08-25 13:04:44 +0530

    CRUD operations

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
-    path('',views.home,name='blog-home' ),
+    path('',PostListView.as_view(),name='blog-home' ),
+    path('post/<int:pk>/',PostDetialView.as_view(),name='post-detial'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
     path('about/',views.about,name='blog-about' ),
-
+    path('post/new/',PostCreateView.as_view(),name='post-create' ),
+    
 ]
