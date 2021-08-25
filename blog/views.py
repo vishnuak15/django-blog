6b2cb1702c1ed4c0fae06bc5047a6c85b3281519
Vishnu <vishnuak15@gmail.com>
2021-08-26 00:17:48 +0530

    User Pagination

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 # Create your views here.
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 from django.views.generic import ListView,DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.models import User
+
 
 def home(request):
     context = {
@@ -17,6 +19,16 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 3
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetialView(DetailView):
     model = Post
