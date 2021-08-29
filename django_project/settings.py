2d9ba4a732661563fbc202a1d963b8d5eb65380d
Vishnu <vishnuak15@gmail.com>
2021-08-29 21:05:10 +0530

    updated changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD':  os.environ.get('PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
